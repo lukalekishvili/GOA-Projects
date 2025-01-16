638360520f90fbef96d444be8e4988aa353fcd8a
lukalekishvili <luka.lekishvili14@gmail.com>
2025-01-16 17:21:11 +0400

    commit

---
+++ day 0/classwork/lesson0.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1: draw a square
+speed(7)
+width(7)
+color("purple")
+begin_fill()
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+end_fill()
+exitonclick()
+#end of square
