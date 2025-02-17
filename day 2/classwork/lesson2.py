ea988e2068ab2bdc7a8f3f329e252d4bb5d34a3e
lukalekishvili <luka.lekishvili14@gmail.com>
2025-02-17 23:26:16 +0400

    commit

---
+++ day 2/classwork/lesson2.py
@@ -0,0 +1,40 @@
+
+
+
+num1 = input("number 1: ")
+num2 = input("number 2: ")
+num3 = input("number 3: ")
+
+num4 = int(num1) + int(num2) + int(num3)
+
+
+print(num4)
+
+saxeli = input("tqveni saxeli: ")
+
+
+print(saxeli)
+
+
+
+
+number_1 = input("tqveni asaki: ")
+number_2 = input("tqveni megobris asaki: ")
+
+print(number_1>number_2)
+
+
+
+word1 = input("surname: ")
+age = input("asaki: ")
+
+print(word1 * int(age))
+
+
+num5 = input("ricxvi1: ")
+num6 = input("ricxvi2: ")
+num6 = int(num6) + 1
+
+print(str(num5) + str(num6))
+
+
