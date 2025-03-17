3e9bf5b714c7b9861f3f12f6aeb2d8ec0a46c521
lukalekishvili <luka.lekishvili14@gmail.com>
2025-03-17 23:55:12 +0400

    commit

---
+++ day 011/classwork/lesson11.py
@@ -0,0 +1,18 @@
+# 1
+# def make_negative( number ):
+#     if number <0:
+#         return number
+#     else:
+#         return -number
+
+# 2
+# def solution(string):
+#     res = ""
+#     for i in string:
+#         res = i + res
+#     return res
+
+
+# 3
+# def number_to_string(num):
+#     return str(num)
--- test.py
+++ test.py
@@ -1,38 +1,6 @@
-# def print_two_times():
-#     return"bye"
-#     print("hello")
-    
-
-# print(print_two_times())
-
-# num = print("hello")
-# print(num)
-
-
-# name1 = "luka"
-# name2 = "lasha"
-
-# def greet( name1, name2 ):
-#     return "hello " + name1 + " and " + name2
-
-# print(greet("deme", "ana"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 name = "luka"
-def say_hello(name):
-    return "Hello, " + name
+def greet(name):
+    return "Hello" + name
 
-print(say_hello("luka"))
-
+result = greet("ucha")
+print(result)
