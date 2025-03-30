ada0abdadfafa9daf660f5598035fda2e14d7c65
lukalekishvili <luka.lekishvili14@gmail.com>
2025-03-30 12:23:13 +0400

    commit

---
+++ day 013/homework/lesson13.py
@@ -0,0 +1,40 @@
+# 1
+
+# def res (num):
+#     x = 0
+#     for i in str(num):
+#         x += int(i)
+#     return num % x
+# print(res(120))
+
+
+# 2
+
+# num = 67
+
+# if num % 2 == 1:
+#     print(num)
+# num += 1
+# print(num)
+
+# 3
+# num = 11
+# res = 0
+# if num %2==0:
+#     res = num+5
+# else: 
+#     res = num * 3
+# res = num % 5
+# print(res)
+
+# 4
+
+# name = "luka"
+# surname = "lekishvili"
+# age = 16
+# nationality = "qartveli"
+
+# print(f"Gmarjoba me var {name} {surname} var {age} wlis da var {nationality}")
+
+
+
