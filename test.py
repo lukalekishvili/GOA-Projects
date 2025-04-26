056947d8b3dc1435179e63c7398f1d4f24b767d5
lukalekishvili <luka.lekishvili14@gmail.com>
2025-04-26 19:32:39 +0400

    commit

--- test.py
+++ test.py
@@ -88,27 +88,17 @@
 
 
 
+# list1 = [1, 7, 3, 23]
+# list2 = [4, 7, 9, 5]
+
+# list3 = []
+# for item in list1:
+#     list3.append(item)
+# for item in list2:
+#     list3.append(item)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-list1 = [1, 7, 3, 23]
-list2 = [4, 7, 9, 5]
-
-list3 = []
-for item in list1:
-    list3.append(item)
-for item in list2:
-    list3.append(item)
