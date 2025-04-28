292e5d5a1873ab0546ab887c99d8d5cb1f7715c3
lukalekishvili <luka.lekishvili14@gmail.com>
2025-04-28 21:22:54 +0400

    commit

---
+++ day 015/homework/lesson15.py
@@ -0,0 +1,41 @@
+# 1
+
+# listt=[1,4,3,6,9,11,17,13,26,30]
+# count1=0
+# count2=0
+# for i in listt:
+#     if i % 2 ==0:
+#         count1+=i
+#     else:
+#         count2+=1
+# print(count2)
+# print(count1)
+
+
+# 2
+
+# listt = [1,4,3,6,9,11,17,13,26,30]
+
+# listt.append(55)
+# print(listt)
+
+# 3
+
+# listt=[1,2,3,3,4,5]
+
+# listt.pop(3)
+# print(listt)
+
+# 4
+
+# listt= [1,2,3,4,6,8,9]
+
+# listt.insert(6,7)
+# print(listt)
+
+#5
+
+
+# append amatebs elements listis boloshi
+# insert amatebs elements listshi mititebul adgilas
+# pop listidan agdebs mititebul elements
