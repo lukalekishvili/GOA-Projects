2b55e5bffb630ddc787acb352f888c2476958a37
lukalekishvili <luka.lekishvili14@gmail.com>
2025-05-18 21:04:09 +0400

    commit

---
+++ day 027/hemowrok/lesson27.py
@@ -0,0 +1,42 @@
+# 1
+
+# def date(day, month, year):
+#     return f"{day}/{month}/{year}"
+
+
+# print(x(5, 14, 2025))
+
+
+# 2
+
+
+# def passw(password):
+#     if len(password) < 8:
+#         return "You Need To Enter At Least 8 Characters"
+#     else:
+#         return "correct password"
+    
+# print(passw("goa112233"))
+
+# 3
+
+# def nums(n1, n2, n3):
+#     l = [n1, n2, n3]
+#     return max(l)
+    
+# print(nums(45, 55, 500))
+
+
+# 4
+
+
+
+# l = []
+# i = 0
+# while i < 5:
+#     x = int(input("Enter Number: "))
+#     l.append(x)
+#     i += 1
+
+# print(l)
+# print(sum(l))
