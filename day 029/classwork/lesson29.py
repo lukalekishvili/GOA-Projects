55bece6291413a5700dab0e72c4523bd90aa4cb9
lukalekishvili <luka.lekishvili14@gmail.com>
2025-05-25 17:08:53 +0400

    commit

---
+++ day 029/classwork/lesson29.py
@@ -0,0 +1,12 @@
+# 1
+
+# link:https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/python
+
+# 2
+
+# link:https://www.codewars.com/kata/57cff961eca260b71900008f/train/python
+
+# 3
+
+# link:https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+
