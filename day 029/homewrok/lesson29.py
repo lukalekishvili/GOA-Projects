3717deed593a26073d841607dd2ed016b5bc4456
lukalekishvili <luka.lekishvili14@gmail.com>
2025-06-01 18:43:08 +0400

    commit

---
+++ day 029/homewrok/lesson29.py
@@ -0,0 +1,18 @@
+# გამოიყენეთ Comprasion Operations და Logical Operations ერთად, შეეცადეთ კომენტარებით ახსნათ რატომ გამოაქვს ესეთი შედეგი და ასევე თუ რომელი სრულდება პირველი, რა თანმიმდევრობით.,
+# comparison_operations=">,<,==,!=,<=,>=,"
+# Logical_operations="and,or,not"
+
+# #comparison operators and logical operators are used to evaluate the conditions and combine expressions.
+
+# #comparison operators are used to compare two values or expression returning boollean value
+# #Purpose: Used to compare values, often in conditional statements like if, while, or loops.
+
+
+# #Logical operators combine Boolean expressions to evaluate multiple conditions, returning a Boolean result (True or False).
+# #Purpose: Used to combine multiple conditions in control structures or to manipulate Boolean values.
+
+
+# #შეეცადოთ გაავლოთ პარალელი დღეს შესწავლილ თემაში და მათემატიკაში (მაგ.: პირველი სრულდება ფრჩხილებში ჩასმული განცხადება).
+# With logical operators, the first condition to be evaluated is "and" due to its higher precedence. For instance, in the expression True or False and True or False, the evaluation proceeds as follows:,
+# True or (False and True) or False → True or False or False → True or False → True.,
+# The similarity between mathematical and logical operators is that in mathematics, the first operations evaluated are typically multiplication or division due to their higher precedence. Similarly, with logical operators, the initial operation evaluated is "and" because it has higher precedence than "or".
